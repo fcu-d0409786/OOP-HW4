66fd8c58e2960dae75417ad5e6542107ba44c399
小胖珊 <@YiShan>
2016-10-25 00:09:42 +0800

    2

--- src/fcu/iecs/oop/password/Main.java
+++ src/fcu/iecs/oop/password/Main.java
@@ -1,5 +1,19 @@
 package fcu.iecs.oop.password;
-
-public class Main {
-
+import java.util.Scanner;
+public class Main 
+{
+	public static void main(String[] args) {
+		
+		String password = null;
+		PasswordEncode pe = new PasswordEncode();
+		do
+		{
+			Scanner input = new Scanner(System.in);
+			System.out.print("Please enter a password:");
+			password = input.next();
+			String newPassword = pe.encode(password);
+			System.out.println("Encode result : " + newPassword);
+		}while(!password.equals("exit"));
+		
+	}
 }
